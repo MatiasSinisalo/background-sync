2526b757919396159577d6d0c3139ad136940d11
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-03 13:37:03 +0300

    backend serves an image at /api/wallpaper

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"log"
+	"os"
+	"path/filepath"
+	"fmt"
+	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello, World!")
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+    imagePath := "./static/wallpaper1.jpeg" // Replace with your image path
+    file, err := os.Open(imagePath)
+    if err != nil {
+        http.Error(w, "Image not found.", http.StatusNotFound)
+        return
+    }
+    defer file.Close()
+
+    // Set the appropriate content type
+    w.Header().Set("Content-Type", "image/jpeg") // or image/png, etc.
+
+    info, _ := file.Stat()
+    // Serve the file content
+    http.ServeContent(w, r, filepath.Base(imagePath), info.ModTime(), file)
 }
 
 func main() {
-    http.HandleFunc("/", helloHandler)
-    fmt.Println("Starting server on :8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println(err)
-    }
+	log.Println("server started..")
+	http.HandleFunc("/api/wallpaper", imageHandler)
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
